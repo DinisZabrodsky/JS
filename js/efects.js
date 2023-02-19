5fb1c4651d1ab82e34a64fd7ec9a164a3d4f97f9
Dinis PC <DinisZabrodsky@gmail.com>
2023-02-19 14:37:53 +0200

    011

--- js/efects.js
+++ js/efects.js
@@ -40,7 +40,7 @@ const Hero = {
 }
 
 // Лічільник ходів, лежить у глобальній області видимості, адже до його будуть звертатись безліч функцій
-let course = 0;
+let course = 5;
 
 // Завершення ходу
 const nextCourse = document.querySelector('.efects-button-move').addEventListener('click', () => {
