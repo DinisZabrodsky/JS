7f48893cc24d3476f1affb9f2acbf80239949be2
Dinis PC <DinisZabrodsky@gmail.com>
2023-02-26 12:14:16 +0200

    оновлення

--- js/efectsV2.js
+++ js/efectsV2.js
@@ -1,3 +1,12 @@
+const baceEffects = {
+   кровотеча: "втрата здоров'я протягому певного часу. Сюди можна впихнути функцію яка буде змінювати характеристики персонажа",
+   втома: "Максимальна кількість витривалості зменшується на певну кількість очків",
+}
+
+// let idUserValue = 0;
+
+// const idUser = () => idUser += 1;
+
 class Pers {
    constructor ({name}) {
       this.name = name;
@@ -7,8 +16,11 @@ class Pers {
 
    putEfects ({type, damage = 0, time = 1}) {
       if (time  > 1) {
+         
          console.log(`${this.name} отримує пошкодження ${damage} від ${type}. Ефект діятиме ще ${time} ходів`)
-         const newEffects = {type, damage, time};
+         console.log(baceEffects[type]);
+         
+         const newEffects = { type, damage, time };
          console.log(newEffects)
          this.effects.push(newEffects)
       }
@@ -17,17 +29,26 @@ class Pers {
 
 }
 
-class Arena {
-   constructor({arena}) {
-      this.arena = arena;
-      this.member = []
-   }
-}
+// class Arena {
+//    constructor({arena}) {
+//       this.arena = arena;
+//       this.member = []
+//    }
+// }
 
 
 
 const valentin = new Pers({name: 'Valentin'});
-console.log(valentin);
-console.log(valentin.efects);
+// console.log(valentin);
+// console.log(valentin.efects);
 valentin.putEfects({type:'кровотеча', damage: 30, time: 5});
 console.log(valentin.effects);
+
+// const arenaOne = new Arena({ arena: 'FirstArena' });
+// console.log(arenaOne);
+// arenaOne.member.push(valentin);
+// console.log(arenaOne);
+
+// arenaOne.member[0].time = 3;
+
+// console.log(valentin);
